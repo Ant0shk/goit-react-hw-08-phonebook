9356c2f19ed14edce3d81e06ae195295e579a150
AntoninaYatsenko <dp130688jaj@gmail.com>
2023-11-21 21:34:53 +0100

    add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { StyledHomeText, Title, WrapperHP } from './Home.styled';
+// import { StyledHomeText, Title, WrapperHP } from './Home.styled';
 import { useSelector } from 'react-redux';
 import { selectLogin, selectUser } from 'redux/selectors';
 
 const Home = () => {
   const isLogin = useSelector(selectLogin);
   const { name } = useSelector(selectUser);
-  return isLogin ? (
-    <WrapperHP>
+  // return isLogin ? (
+  //   <WrapperHP>
       
-    </WrapperHP>
-  ) : (
-    <>
+  //   </WrapperHP>
+  // ) : (
+  //   <>
       
-    </>
-  );
+  //   </>
+  // );
 };
 export default Home;

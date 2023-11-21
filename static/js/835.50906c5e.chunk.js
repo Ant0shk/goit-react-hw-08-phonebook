6b0c23f492fe210ab3d1e5347d2d2bede79a022e
Ant0shk <131529631+Ant0shk@users.noreply.github.com>
2023-11-21 20:51:32 +0000

    Deploying to gh-pages from @ Ant0shk/goit-react-hw-08-phonebook@3fa730dedcbb796e73e511648f35f85753440e24 🚀

---
+++ static/js/835.50906c5e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[835],{5835:function(e,t,r){r.r(t);r(2791);var a=r(9434),c=r(7689),n=r(6351),o=r(184);t.default=function(e){var t=e.children;return(0,a.v9)(n.KN)?(0,o.jsx)(c.Fg,{to:"/contacts"}):t}}}]);
+//# sourceMappingURL=835.50906c5e.chunk.js.map
